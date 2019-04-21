5cca078e917a1366316e899034b6a1e9c5c8d163
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-04-21 15:27:53 +0300

    set up the home-screen component and routing component

--- imageSlider/src/app/app.module.ts
+++ imageSlider/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { AppRoutingModule } from './app.routing'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
-    Ng2CarouselamosModule
+    Ng2CarouselamosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ imageSlider/src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {RouterModule ,Routes} from '@angular/router';
+
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+
+const appRoutes : Routes = [
+    {
+        path:"home",
+        component:HomeScreenComponent
+    }
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
