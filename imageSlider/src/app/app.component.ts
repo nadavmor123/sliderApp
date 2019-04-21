6b9506ecbe43d7098781512898605e4b8eeeb5e7
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-04-21 13:45:19 +0300

    slider v1 - has annoying bugs! i'll go for ngx-bootstrap in separate branch

--- imageSlider/src/app/app.component.ts
+++ imageSlider/src/app/app.component.ts
@@ -10,10 +10,29 @@ export class AppComponent {
   items: Array<any> = []
 
   constructor() {
-    this.items = [
-      { name: 'assets/images/thumb1.jpg' },
-      { name: 'assets/images/thumb2.jpg' },
-      { name: 'assets/images/thumb3.jpg' }
-    ]
+    this.resetImages();
   }
+
+  public reIndexImages(): void {
+		this.items.forEach( (item,newIndex) => {
+        item.index = newIndex;
+    });
+  }
+
+  public removeImage(index): void {
+    this.items.splice(index,1);
+  }
+  
+  public duplicateImage(catalogNum): void {
+    var slide = { name: 'assets/images/thumb'+catalogNum+'.jpg' ,catalog:catalogNum ,index :this.items.length }
+    this.items.push(slide);
+  }
+  
+  public resetImages(): void {
+		this.items = [
+      { name: 'assets/images/thumb1.jpg',catalog:1,index:0 },
+      { name: 'assets/images/thumb2.jpg',catalog:2,index:1  },
+      { name: 'assets/images/thumb3.jpg',catalog:3 ,index:2 }
+    ]
+	}
 }
