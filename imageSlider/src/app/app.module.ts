77c1dd30a1c7c974e7487cf08b7c0a603bcfce5d
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-04-24 12:20:45 +0300

    HUGE "ngx-bootstrap/index.d.ts, found version 4, expected 3" bug - took 2 hours to solve!! where did that come from?? :(

--- imageSlider/src/app/app.module.ts
+++ imageSlider/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { AppRoutingModule } from './app.routing'
@@ -13,7 +12,6 @@ import { AppRoutingModule } from './app.routing'
   ],
   imports: [
     BrowserModule,
-    Ng2CarouselamosModule,
     AppRoutingModule,
     CarouselModule.forRoot()
   ],
